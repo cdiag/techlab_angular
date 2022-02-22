c18836efb7f030ccb3e7b0731237c2d25de7f3bc
Dominik Hellweg <dominik.hellweg@cdi-ag.de>
2022-02-22 08:23:35 +0100

    Add CRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {HttpClient} from '@angular/common/http';
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {Book} from '../models/book';
 
 @Component({
@@ -7,20 +7,43 @@ import {Book} from '../models/book';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   books: Book[] = [];
+  author: string;
+  title: string;
 
   constructor(private http: HttpClient) {
   }
 
-  ngOnInit(): void {
-    this.loadBooks();
+  loadBooks(): void {
+    this.http.get<Book[]>('http://localhost:3000/books').toPromise().then(data => {
+      this.books = data;
+    });
   }
 
-  loadBooks(): void {
-    this.http.get<Book[]>('http://localhost:3000/books').subscribe(data => {
-      console.log('load books response', data);
-      this.books = data;
+  addBook(): void {
+    this.http.post('http://localhost:3000/books', {
+      author: this.author,
+      title: this.title
+    }).toPromise().then(() => {
+      this.author = null;
+      this.title = null;
+      this.loadBooks();
+    });
+  }
+
+  updateBook(book: Book): void {
+    this.http.put('http://localhost:3000/books/' + book._id, {
+      author: book.author,
+      title: book.title
+    }).toPromise().then(() => {
+      this.loadBooks();
+    });
+  }
+
+  deleteBook(book: Book): void {
+    this.http.delete('http://localhost:3000/books/' + book._id).toPromise().then(() => {
+      this.loadBooks();
     });
   }
 }
