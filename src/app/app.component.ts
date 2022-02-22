92e40b5a858fe3269de396f764042ddc5a8d8f39
Dominik Hellweg <dominik.hellweg@cdi-ag.de>
2022-02-22 07:29:14 +0100

    Add model book

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {Book} from '../models/book';
 
 @Component({
   selector: 'app-root',
@@ -7,16 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  books = [];
+  books: Book[] = [];
 
   constructor(private http: HttpClient) {
-    
   }
+
   ngOnInit(): void {
-    this.http.get<[]>("http://localhost:3000/books").subscribe( data => {
-      this.books = data
-    })
+    this.loadBooks();
   }
 
-
+  loadBooks(): void {
+    this.http.get<Book[]>('http://localhost:3000/books').subscribe(data => {
+      console.log('load books response', data);
+      this.books = data;
+    });
+  }
 }
---
+++ src/models/book.ts
@@ -0,0 +1,6 @@
+export class Book {
+  // tslint:disable-next-line:variable-name
+  _id: string;
+  author: string;
+  title: string;
+}
